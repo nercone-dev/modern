cbb9975271463a3fe44c81e0fc0279906eba61d8
Nercone <diamondgotcat@gmail.com>
2025-10-29 15:14:32 +0900

    Update __main__.py

--- nercone_modern/__main__.py
+++ nercone_modern/__main__.py
@@ -4,10 +4,11 @@ if __name__ == "__main__":
     import time
 
     logger = NerconeModern().modernLogging("Main")
+    logger.log("This is a debug message", "DEBUG")
     logger.log("This is a test message", "INFO")
     logger.log("This is a warning message", "WARNING")
     logger.log("This is an error message", "ERROR")
-    logger.log("This is a debug message", "DEBUG")
+    logger.log("This is an error message", "CRITICAL")
 
     progress_bar1 = NerconeModern().modernProgressBar(total=100, process_name="Task 1", process_color=32, spinner_mode=False)
     progress_bar1.setMessage("WAITING")
